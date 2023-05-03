50c0399d30ffcce519fc6bf335a5e8995e1485fa
henriquepadua <henrique.lommes@gmail.com>
2023-05-02 22:31:59 -0300

    primeiro componente com angular-cli

---
+++ PrimeiraAPI/jedi-academy/src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jad-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  construtor(){}
+
+  ngOnInit(){
+    
+  }
+}
