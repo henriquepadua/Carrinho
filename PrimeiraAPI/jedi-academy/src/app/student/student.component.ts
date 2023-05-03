6a344fced8a651685ac712777b1edac0a6563fe1
henriquepadua <henrique.lommes@gmail.com>
2023-05-02 22:45:39 -0300

    Aplicando Property Binding nos Primeiros Componentes

---
+++ PrimeiraAPI/jedi-academy/src/app/student/student.component.ts
@@ -0,0 +1,16 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jad-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent {
+  name: string = 'Luke'
+  isJedi: boolean = true
+
+  constructor(){}
+
+  ngOnInit(){
+  }
+}
